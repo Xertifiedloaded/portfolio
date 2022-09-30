b1e0f5f485218bc47e4b1264983767f3e4320db5
Makinde Olaitan <certifiedloaded@yahoo.com>
2022-09-30 16:28:46 +0100

    my changes

---
+++ assets/scripts/script.js
@@ -0,0 +1,15 @@
+let options = {
+    strings: ["front-End development", "web development", "crypo analysing"],
+    typeSpeed: 150,
+    backSpeed: 100,
+    loop: true
+};
+let typed = new Typed('.typing', options);
+
+// or use  it inside html script with help of CDN
+// let typed = new Typed(".typing", {
+//     strings: ["front-End development", "web development", "crypo analysing"],
+//     typeSpeed: 150,
+//     backSpeed: 150,
+//     loop: true
+// })
