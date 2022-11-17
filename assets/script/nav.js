b54a79cdb681687829dcae9689718c7e8efdf3ca
xertifiedloaded <certifiedloaded@yahoo.com>
2022-11-17 20:08:35 +0100

    my changes

--- assets/script/nav.js
+++ assets/script/nav.js
@@ -2,20 +2,45 @@ const show = document.querySelector(".show")
 const hide = document.querySelector(".hide")
 const hamburger = document.querySelector(".hamburger")
 const solid = document.querySelector(".fa-solid")
+let slideNav = document.querySelector(".nav")
+console.log(slideNav)
 let isActive = false
 function collapseMenu() {
     if (isActive == false) {
         isActive = true
         hide.classList.replace("hide", "show")
         solid.classList.replace("fa-bars", "fa-xmark")
+        // slideNav.style.transform=" translateY(-100%)"
     } else {
         isActive = false
         hide.classList.replace("show", "hide")
         solid.classList.replace("fa-xmark", "fa-bars")
-
     }
 }
+
 hamburger.addEventListener("click", collapseMenu)
+// animate on scroll
+let content = document.querySelectorAll(".display")
+// let contentPosition = content.getBoundingClientRect()
+// console.log(screenPosition)
+// console.log(content)
+window.addEventListener("scroll", () => {
+
+    content.forEach(windowContent => {
+        let screenPosition = window.innerHeight / 5 * 4
+        // console.log(screenPosition)
+        let contentPosition = windowContent.getBoundingClientRect().top
+        // console.log(contentPosition)
+        if (contentPosition < screenPosition) {
+            windowContent.classList.add("active")
+            console.log("were ti sise")
+        } else {
+            windowContent.classList.remove("active")
+            console.log("this were no work sha")
+        }
+    })
+})
+// change header color on scroll
 let nav = document.querySelectorAll(".nav-link")
 let header = document.querySelector(".header")
 let logo = document.querySelector(".logo")
@@ -23,23 +48,24 @@ window.addEventListener("scroll", () => {
     console.log(window.scrollY)
     if (window.scrollY >= 50) {
         header.classList.add("active-header")
-        logo.style.color="black"
+        logo.style.color = "black"
         nav.forEach(navLink => {
             navLink.style.color = "black"
             navLink.style.fontWeight = "500"
         })
-        console.log("y")
+        // console.log("y")
     }
     else {
-        logo.style.color="white"
+        logo.style.color = "white"
         header.classList.remove("active-header")
         nav.forEach(navLink => {
             navLink.style.color = "white"
         })
-        console.log("x")
+        // console.log("x")
     }
 })
-// 
+
+// automatic slider
 let slider = document.querySelector(".slider1")
 let images = new Array(
     "assets/images/cttaste.png",
